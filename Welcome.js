61b3b1d22e3b8ca8fade868bfe296f29d0edeb0b
Ryan Foss <32232796+rfoss92@users.noreply.github.com>
2018-09-12 10:58:39 -0400

    Add files via upload

--- Welcome.js
+++ Welcome.js
@@ -1,54 +1,56 @@
-(weather => {
 
-  // Time
-  let date = new Date();
-  let hour = date.getHours();
-  hour = (hour < 10 ? "0" : "") + hour;
-  let min  = date.getMinutes();
-  min = (min < 10 ? "0" : "") + min;
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-  let day  = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
-  date = month + "/" + day + "/" + year;
-  let time = hour + ":" + min;
+(getTime => {
 
-if (hour > 5 && hour <= 12) {
-  $('#greeting').text('Good Morning!');
-  document.body.style.backgroundImage = "url('img/morning.jpg')";
-} else if (hour > 12 && hour <= 18) {
-  $('#greeting').text('Good Afternoon!');
-  document.body.style.backgroundImage = "url('img/afternoon.jpg')";
-} else if (hour > 18 && hour <= 21) {
-  $('#greeting').text('Good Evening!');
-  document.body.style.backgroundImage = "url('img/evening.jpg')";
-} else if (hour > 21 || hour <= 5) {
-  $('#greeting').text('Good Night!');
-  document.body.style.backgroundImage = "url('img/night.jpg')";
-}
+  (setTime => {
+    let date = new Date();
+    let hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    let min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    let time = hour + ":" + min;
+    $("#time").html(time);  
+    setGreeting(hour);  
+  })();
 
-  // GeoLocation
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(sendRequest);
-  } else {
-    $("#description").html("Geolocation is not supported by this browser.");
+  (setDate => {
+    let date = new Date();  
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    let day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    let currentDate = month + "/" + day + "/" + year;
+    $("#date").html(currentDate);    
+  })();
+
+  function setGreeting(hour){
+    let greeting;
+    (5 < hour && hour <= 12) ? (greeting = 'Morning') 
+      : (12 < hour && hour <= 18) ? (greeting = 'Afternoon')
+      : (18 < hour && hour <= 21) ? (greeting = 'Evening')
+      : (greeting = 'Night');
+    $('#greeting').text(`Good ${greeting}!`);
+    $('body').css('background-image', `url('img/${greeting}.jpg')`); 
   }
+})();
 
-  // Get Weather Data
+(weather => {
+  (navigator.geolocation) ? navigator.geolocation.getCurrentPosition(sendRequest)
+    : $("#description").html("Geolocation is not supported by this browser.");
+
   function sendRequest(position) {
-    let url = "https://fcc-weather-api.glitch.me/api/current?lat="
-      + position.coords.latitude + "&lon=" + position.coords.longitude;
-
+    let url = "http://api.openweathermap.org/data/2.5/forecast?lat="
+      + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=1815c1cb674522f41e5935a2267ee5b6";
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = () => {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         let data = JSON.parse(xmlhttp.responseText);
-        let weather = {};
-        weather.description = data.weather[0].description;
-        weather.temp = data.main.temp;
-        weather.humidity = data.main.humidity;
-        weather.wind = data.wind.speed;
+        let weather = {
+          description: data.list[0].weather[0].description,
+          temp: data.list[0].main.temp,
+          humidity: data.list[0].main.humidity,
+          wind: data.list[0].wind.speed
+        }
         update(weather);
       }
     };
@@ -56,56 +58,45 @@ if (hour > 5 && hour <= 12) {
     xmlhttp.send();
   }
 
-  // Update HTML
   function update(weather) {
-    
-    let fahrenheit = weather.temp * 9 / 5 + 32;
+    let fahrenheit = ((weather.temp-273.15)*1.8)+32;
+    let celsius = weather.temp-273.15;
     fahrenheit = Math.round(fahrenheit);
-
-    $("#date").html(date);    
-    $("#time").html(time);      
+    celsius = Math.round(celsius);
     $("#description").html(weather.description + '<hr>');
-    $("#temp").html(fahrenheit + "<sup>°F</sup>, " + weather.temp + "<sup>°C</sup><hr>");
+    $("#temp").html(fahrenheit + "<sup>°F</sup>, " + celsius + "<sup>°C</sup><hr>");
     $("#humidity").html("Humidity: " + weather.humidity + "%<hr>");
     $("#wind").html("Wind: " + weather.wind + " mph<hr>");
   }
-
 })();
 
 (quote => {
-  var randomQuote = "";
-  var randomAuthor = "";
-
-  $("#quoteButton").on("click", function() {
-    var url =
-      "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-    $.getJSON(url, function(data) {
+  let randomQuote;
+  let randomAuthor;
+  $("#quoteButton").click(() => {
+    let url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+    $.getJSON(url, (data) => {
       $("#quote-content").html('" ' + data.quoteText + ' "');
       $("#quote-author").html(" - " + data.quoteAuthor);
-      randomQuote = document.getElementById("quote-content").textContent;
-      randomAuthor = document.getElementById("quote-author").textContent;
+      randomQuote = $("#quote-content").text();
+      randomAuthor = $("#quote-author").text();
     });
   });
-
-  $("#tweetLink").on("click", function() {
-    window.open(
-      "https://twitter.com/intent/tweet?text=" + randomQuote + randomAuthor
-    );
-  });
+  $("#tweetLink").click(() => open("https://twitter.com/intent/tweet?text=" + randomQuote + randomAuthor));
 })();
 
 (wiki => {
-  $("form").on("submit", function(e){
+  $("#wikiForm").on("submit", (e) => {
   e.preventDefault();
-    var searchTerm = $("#searchTerm").val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+    let searchTerm = $("#searchTerm").val();
+    let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
     $.ajax({
       url: url,
       type: "GET",
       async: false,
       dataType: "json",
-      success: function(data, status, jqXHR){
-        for(var i = 1; i < 6; i++) {
+      success: (data, status, jqXHR) => {
+        for(let i = 1; i < 6; i++) {
           $("#output").append(
             "<div class='well'><a href=" 
             + data[3][i] 
@@ -116,6 +107,6 @@ if (hour > 5 && hour <= 12) {
             + "</p></a></div>");
         }
       }
-    })
+    });
   })
 })();
